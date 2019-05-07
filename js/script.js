b2ba6fc421fe092064a58825eb92949114c23b8f
JolantaHuba <huba.jolanta@gmail.com>
2019-05-07 18:44:56 +0200

    Slider done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-let burgerIcon = document.querySelector('.nav__burger');
-let navList = document.querySelector('.nav__list');
+const burgerIcon = document.querySelector('.nav__burger');
+const navList = document.querySelector('.nav__list');
 
 burgerIcon.addEventListener('click', function () {
     burgerIcon.classList.toggle('active')
     navList.classList.toggle('active')
 })
+
+// DODAC ZAMYKANIE PO KLIKNIECIU LINKU W MENU
+
+// SLIDER
+new Glide('.glide', {
+    type: 'carousel',
+}).mount()
 
 // burgerIcon.addEventListener('click', function () {
 //     burgerLines.forEach(line => {
