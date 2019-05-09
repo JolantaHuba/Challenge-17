d81d55e88adf2e1f58d4cd7be5d9941140060bca
JolantaHuba <huba.jolanta@gmail.com>
2019-05-09 22:19:04 +0200

    Gallery finished

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 const burgerIcon = document.querySelector('.nav__burger');
 const navList = document.querySelector('.nav__list');
+const Links = document.querySelectorAll('a')
 
 burgerIcon.addEventListener('click', function () {
     burgerIcon.classList.toggle('active')
     navList.classList.toggle('active')
 })
 
-// DODAC ZAMYKANIE PO KLIKNIECIU LINKU W MENU
+navList.addEventListener('click', function () {
+    navList.classList.remove('active')
+    burgerIcon.classList.remove('active')
+})
+
+// scroll, animation offset... instead scrollIntoView
+
+// Links.forEach(link => {
+//     link.addEventListener('click', function () {
+//         let goTo = this.getAttribute('href').replace('#', '.')
+//         console.log(goTo)
+//         document.querySelector(goTo).scrollIntoView()
+//     })
+// });
 
 // SLIDER
 new Glide('.glide', {
