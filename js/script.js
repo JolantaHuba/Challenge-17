9d4d30505d2616fabac3003a4632884e72ab005d
JolantaHuba <huba.jolanta@gmail.com>
2019-05-10 20:50:40 +0200

    Blog and contact finished
    Fix a few issues, optimize code

--- js/script.js
+++ js/script.js
@@ -14,13 +14,14 @@ navList.addEventListener('click', function () {
 
 // scroll, animation offset... instead scrollIntoView
 
-// Links.forEach(link => {
-//     link.addEventListener('click', function () {
-//         let goTo = this.getAttribute('href').replace('#', '.')
-//         console.log(goTo)
-//         document.querySelector(goTo).scrollIntoView()
-//     })
-// });
+Links.forEach(link => {
+    link.addEventListener('click', function () {
+        let goTo = this.getAttribute('href').replace('#', '.')
+        let howFar = document.querySelector(goTo).offsetTop - 60
+        window.scroll(0, howFar)
+        // document.querySelector(goTo).scrollIntoView(true)
+    })
+})
 
 // SLIDER
 new Glide('.glide', {
